5c50378eb04cd338337d9690b9cf5577baf0a885
email <shresthadas.0.infinity@gmail.com>
2026-02-07 08:20:28 +0000

    into_iter, iter, iter_mut

---
+++ hello_rust/examples/iterator.rs
@@ -0,0 +1,17 @@
+#![allow(unused)]
+
+// Iterators
+fn main() {
+    let vals: Vec<u32> = vec![1, 2, 3, 4, 5];
+    // into_iter ---- iterate T
+    // iter      ---- iterate &T  -- can call again
+    // iter_mut  ---- iterate &mut T -- can call again
+
+    for v in vals.iter() {
+        //
+    }
+
+    for v in vals.iter() {
+        // 
+    }
+}
---
+++ hello_rust/target/debug/examples/iterator-0c31d631a8ea6bcf.d
@@ -0,0 +1,5 @@
+/mnt/c/Users/shres/Desktop/RUST/LearnRust/hello_rust/target/debug/examples/iterator-0c31d631a8ea6bcf.d: examples/iterator.rs
+
+/mnt/c/Users/shres/Desktop/RUST/LearnRust/hello_rust/target/debug/examples/iterator-0c31d631a8ea6bcf: examples/iterator.rs
+
+examples/iterator.rs:
